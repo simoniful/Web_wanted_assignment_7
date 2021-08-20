fdabe35f340a6325e6dffaa20c7e9543173da17d
sanghunlee <simoniful0gmail.com>
2021-08-20 18:06:59 +0900

    Refactor: Todofooter 분기처리

--- src/components/todo/template/footer/TodoFooter.tsx
+++ src/components/todo/template/footer/TodoFooter.tsx
@@ -21,7 +21,7 @@ interface HooksTodoHeadProps {
 }
 
 const Todofooter = ({ todos }: HooksTodoHeadProps) => {
-  const undoneTasks = todos.filter((todo) => !todo.done);
+  const undoneTasks = todos?.filter((todo) => !todo.done);
   return (
     <TodoFooterBlock>
       <LeftText className="tasks-left">{undoneTasks.length} items left</LeftText>
